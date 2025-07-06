3a50cc0db2e95a1b57272ddfbbb2db2541fe662b
Phạm Công Tính <84382773+TinhSoMa@users.noreply.github.com>
2025-07-06 15:01:22 +0700

    Update index.php

--- functions/simple-test/index.php
+++ functions/simple-test/index.php
@@ -1,23 +1,14 @@
-<?php
-
 use Utopia\App;
 use Utopia\CLI\Console;
 
-App::init(function (array $utopia, array $request, array $response, array $args) {
-    /* 
-     * Simple Test Function
-     * 
-     * Function đơn giản để test kết nối Appwrite
-     */
-    
+App::init(function (array $utopia, array $request, array &$response, array $args) {
     Console::log('Simple test function started');
-    
+
     $payload = $request['payload'] ?? '{}';
     $data = json_decode($payload, true) ?: [];
-    
+
     Console::log('Received payload: ' . $payload);
-    
-    // Simple response
+
     $result = [
         'success' => true,
         'message' => 'Simple test function executed successfully',
@@ -26,13 +17,9 @@ App::init(function (array $utopia, array $request, array $response, array $args)
         'function_id' => '686a1e4a0010de76b3ea',
         'test' => true
     ];
-    
-    Console::log('Function completed successfully');
-    
-    return $result;
-    
-}, ['utopia', 'request', 'response', 'args']);
 
-App::shutdown(function (array $utopia, array $request, array $response, array $args) {
-    Console::log('Simple test function shutdown');
-}, ['utopia', 'request', 'response', 'args']); 
+    Console::log('Function completed successfully');
+
+    $response['json'] = $result;
+
+}, ['utopia', 'request', 'response', 'args']);
